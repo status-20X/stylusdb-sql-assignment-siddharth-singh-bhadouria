2499b9b99cec14fad2b773f76a5aed8ef1030270
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2024-04-11 02:47:21 +0530

    Step 4

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const parseQuery = require('./queryParser');
+const readCSV = require('./csvReader');
+
+async function executeSELECTQuery(query) {
+    const { fields, table } = parseQuery(query);
+    const data = await readCSV(`${table}.csv`);
+    
+    return data.map(row => {
+        const filteredRow = {};
+        fields.forEach(field => {
+            filteredRow[field] = row[field];
+        });
+        return filteredRow;
+    });
+}
+
+module.exports = executeSELECTQuery;
